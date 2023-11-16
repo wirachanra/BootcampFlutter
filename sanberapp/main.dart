6873399456276ba287b120e557e43c7fd73648a3
Wira Chanra <wira.chanra@lixus.id>
2023-11-16 17:13:43 +0700

    flutter intro

---
+++ sanberapp/main.dart
@@ -0,0 +1,15 @@
+double sum(double a, double b) {
+  return a + b;
+}
+
+double sqare(double x) => x * x;
+
+void main() {
+  for (var i = 2; i <= 10; i++) {
+    print(i);
+  }
+}
+
+// printMessage(String message, String from, [String to = 'unknow']) {
+//   print('"$message", from ${from} to ${to}');
+// }
